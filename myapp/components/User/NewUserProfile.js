8ecdcaaccce23a32dcbce5e6c18e6d99b52a735e
Karan <karanbhanu788@gmail.com>
2023-12-29 16:36:01 +0530

    handling new user profile

--- myapp/components/User/NewUserProfile.js
+++ myapp/components/User/NewUserProfile.js
@@ -1,8 +1,12 @@
-import { StyleSheet, Text, View,TextInput,TouchableOpacity,SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View,TextInput,TouchableOpacity,SafeAreaView,ScrollView, Platform, Image } from 'react-native'
 import React,{useState} from 'react' 
 import { useNavigation } from '@react-navigation/native'
 import axios from 'axios' 
 import Toast from 'react-native-toast-message'
+import Navbar from '../MainContent/Navbar'
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome' 
+import {faUser} from '@fortawesome/free-solid-svg-icons'
+import ImagePicker from 'react-native-image-picker'
 
 const NewUserProfile = () => {
   const navigator = useNavigation()
@@ -22,11 +26,43 @@ const NewUserProfile = () => {
   const [userWorkTenure,setUserWorkTenure] = useState('') ;
   const [projectName,setProjectName] = useState('') ;
   const [projectDescription,setProjectDescription] = useState('') ;
-  const  [projectLink,setProjectLink] = useState('')
+  const [projectLink,setProjectLink] = useState('') ;
+  const [showInputForms1 , setShowInputForms1] = useState(false) ;
+  const [showInputForms2 , setShowInputForms2] = useState(false) ;
+  const [showInputForms3 , setShowInputForms3] = useState(false) ;
+   
+  const handleUploadPhoto = () => {
+     const options = {
+          noData:true ,
+     }
   
-  const handleRegisterButtonClick = async() => {
+  ImagePicker.launchImageLibrary(options,(response) => {
+      if(response.uri){
+          setUserImage(response)
+      }
+  })
+  const data = new FormData()
+  data.append('photo',{
+     name:userImage.fileName,
+     type:userImage.type,
+     uri:Platform.OS === 'android' ? photo.uri : photo.replace('file://','') ,
+  })
+} 
+ const handleUserClickInput1 = () => {
+      setShowInputForms1(!showInputForms1)
+ }
+
+ const handleUserClickInput2 = () => {
+      setShowInputForms2(!showInputForms2)
+ }
+
+ const handleUserClickInput3 = () => {
+      setShowInputForms3(!showInputForms3)
+ }
+
+  const handleNewUserProfile = async() => {
         try {
-             const postResponse = await axios.post("http://192.168.43.148:3500/v1/api/postUserRegister",{
+             const postResponse = await axios.post("http://192.168.43.148:3500/v8/api/postUserProfile",{
                     userImage:userImage,
                     userFullName:userFullName,
                     userJobTitle:userJobTitle,
@@ -34,6 +70,10 @@ const NewUserProfile = () => {
                     userSkills:userSkills,
                     userPreferredLocation:userPreferredLocation,
                     userPreferredLocationType:userPreferredLocationType,
+                    userUniversityName:userUniversityName,
+                    userDegreeName:userDegreeName,
+                    userDegreeTime:userDegreeTime,
+                    userDegreeGPA:userDegreeGPA,
                     userCompanyName:userCompanyName,
                     userWorkDescription:userWorkDescription,
                     userWorkTenure:userWorkTenure,
@@ -57,25 +97,80 @@ const NewUserProfile = () => {
              Toast.show('Server side error occured') 
         } 
   }   
-  return (
+  return ( 
+     <ScrollView>
+     <Navbar/>     
     <View style = {styles.mainContainerRegister}>
          <View style = {styles.registerContainer}>
          <View>
-            <Text style={styles.registrationHeading}>Enter your registration details</Text> 
+            <Text style={styles.registrationHeading}>Complete your profile details</Text> 
           </View> 
-              <TextInput type='text' placeholder="Enter your username" style = {styles.registerTextInput} placeholderTextColor='yellow' value = {userName} onChangeText = {(text) => setUserName(text)}   />
-              <TextInput keyboardType='email-address' placeholder="Enter your email address" style = {styles.registerTextInput2} placeholderTextColor='yellow' value = {userEmail} onChangeText = {(text) => setUserEmail(text) }  />
-              <TextInput type='password' placeholder="Enter password" style = {styles.registerTextInput3} placeholderTextColor='yellow' secureTextEntry={true} value = {userPassword} onChangeText = {(text) => setUserPassword(text)}   />
-              <TouchableOpacity style={styles.registerButton} onPress={handleRegisterButtonClick}>
-                   <Text style = {styles.registerButtonText}>Register</Text>
-              </TouchableOpacity>
-              <View>
-               <TouchableOpacity onPress={() => navigator.navigate("Login")}>
-                  <Text style={styles.loginHeading}>Already have an account Login Here!</Text>
-               </TouchableOpacity>
+              <Image 
+                source = {{uri:userImage.uri}}
+                style = {styles.imageStyling}
+               />
+               <View>
+             {/*  <TouchableOpacity style = {styles.registerButton}>
+                    <Text style = {styles.registerButtonText}>Select</Text>
+  </TouchableOpacity>*/}
+               <TouchableOpacity style = {styles.registerButton} onPress={handleUploadPhoto} >
+                    <Text style = {styles.registerButtonText}>Upload</Text>     
+               </TouchableOpacity> 
                </View>
+              <TextInput type='text' placeholder="Enter your username" style = {styles.registerTextInput} placeholderTextColor='yellow' value = {userFullName} onChangeText = {(text) => setUserFullName(text)}   />
+              <TextInput type='text' placeholder="Enter job role" style = {styles.registerTextInput2} placeholderTextColor='yellow' value = {userJobTitle} onChangeText = {(text) => setUserJobTitle(text) }  />
+              <TextInput type='text' placeholder="Describe about yourself" style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {userProfileDescription} onChangeText = {(text) => setUserProfileDescription(text)}   />
+              <TextInput type='text' placeholder="Enter your skills" style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {userSkills} onChangeText = {(text) => setUserSkills(text)}   />
+              <TextInput type='text' placeholder="Enter your preferred location" style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {userPreferredLocation} onChangeText = {(text) => setUserPreferredLocation(text)}   />
+              <TextInput type='text' placeholder="Location Type - Remote/In-Office" style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {userPreferredLocationType} onChangeText = {(text) => setUserPreferredLocationType(text)}/>
+              <View>
+                    <TouchableOpacity onPress={handleUserClickInput1}>
+                    <Text style = {styles.formText}>     
+                    Add educational details
+                    </Text>
+                    </TouchableOpacity> 
+                    { showInputForms1 && (
+                    <View>
+                       <TextInput type='text' placeholder="Enter university name" style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {userUniversityName} onChangeText = {(text) => setUserUniversityName(text)}   />
+                       <TextInput type='text' placeholder="Enter degree name" style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {userDegreeName} onChangeText = {(text) => setUserDegreeName(text)}   />
+                       <TextInput type='text' placeholder="Enter graduation completion year Ex .2021" style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {userDegreeTime} onChangeText = {(text) => setUserDegreeTime(text)}   />
+                    </View>
+                     )}
+              </View>
+              <View>
+                  <TouchableOpacity onPress={handleUserClickInput2}>
+                  <Text style = {styles.formText}>     
+                    Add Work Experience
+                    </Text>
+                  </TouchableOpacity> 
+                   { showInputForms2 && (
+                   <View>
+                   <TextInput type='text' placeholder="Enter user company name" style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {userCompanyName} onChangeText = {(text) => setUserCompanyName(text)}   />
+                   <TextInput type='text' placeholder="Enter your work description " style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {userWorkDescription} onChangeText = {(text) => setUserWorkDescription(text)}   />
+                   <TextInput type='number' placeholder="Enter your work tenure" style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {userWorkTenure} onChangeText = {(text) => setUserWorkTenure(text)}   />
+                   </View>
+                    )}  
+              </View>
+              <View>
+                 <TouchableOpacity onPress = {handleUserClickInput3}>
+                    <Text style = {styles.formText}>
+                    Add project details     
+                    </Text>
+                 </TouchableOpacity>
+                  { showInputForms3 && ( 
+                  <View>  
+                  <TextInput type='text' placeholder="Enter your project name"  style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {projectName} onChangeText = {(text) => setProjectName(text)} />
+                  <TextInput type='text' placeholder="Descibe about the project" style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {projectDescription} onChangeText = {(text) => setProjectDescription(text)}   />
+                  <TextInput type='text' placeholder="Enter project link" style = {styles.registerTextInput3} placeholderTextColor='yellow' value = {projectLink} onChangeText = {(text) => setProjectLink(text)}   />
+                   </View>
+              )} 
+              </View>
+              <TouchableOpacity style={styles.registerButton} onPress={handleNewUserProfile}>
+                   <Text style = {styles.registerButtonText}>Save</Text>
+              </TouchableOpacity>
          </View>
     </View>
+    </ScrollView>
 
   )
 }
@@ -86,17 +181,17 @@ const styles = StyleSheet.create({
      mainContainerRegister : {
           backgroundColor:'rgba(64,64,64,1)' ,
           height:'100%' ,
-          width:'100%'
+          width:'100%',
+          paddingBottom : 200, 
      },
      registerContainer : {
           backgroundColor:'black' ,
-          marginLeft:45,
-          marginRight:45,
+          marginLeft:15,
+          marginRight:15,
           color:'yellow' ,
-          marginTop:85,
-          height:320,
+          marginTop:40,
+          height:'cover',
           borderRadius:15          
-
      },
      registerTextInput : {
           width:45 ,
@@ -104,7 +199,7 @@ const styles = StyleSheet.create({
           width:'80%',
           borderWidth:1,
           borderColor:'white',
-          marginTop:30,
+          marginTop:8,
           marginLeft:25,
           borderRadius:10,
           color:'yellow',
@@ -139,17 +234,18 @@ const styles = StyleSheet.create({
 } ,
      
      registerButton:{
-          width:100,
+          width:110,
           backgroundColor:'blue',
-          marginLeft:80,
-          marginRight:80,
+          marginLeft:100,
+          marginRight:100,
           borderRadius:5,
           justifyContent:'center',
           alignItems:'center',
           marginTop:20,
-          fontSize:25,
+          fontSize:35,
           color:'white',
-          height:35
+          height:35,
+          marginBottom:15
 
      } ,
 
@@ -159,7 +255,7 @@ const styles = StyleSheet.create({
  
      registrationHeading:{
           color:'yellow',
-          fontSize:15,
+          fontSize:18,
           paddingTop:20,
           paddingLeft:30
      },
@@ -168,6 +264,29 @@ const styles = StyleSheet.create({
           fontSize:15,
           paddingTop:20,
           paddingLeft:16
+     },
+     formText : {
+          fontSize:15,
+          color:'yellow',
+          marginTop:12,
+          marginLeft:30,
+     },
+     imageStyling : {
+          borderColor:'yellow',
+          width:150,
+          height:150,
+          borderWidth:2 ,
+          marginTop:20,
+          marginLeft:75,
+          marginRight:60,
+          borderRadius:75
+     } ,
+     buttonCollection:{
+          display:'flex' ,
+          flexDirection:'row' ,
      }
+     
+
+
 
 })
